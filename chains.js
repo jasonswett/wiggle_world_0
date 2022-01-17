ec4c287b3646e4e530134fd02f09cc0bfcb91d74
Jason Swett <jason@mhcare.com>
2022-01-16 19:11:11 -0500

    Remove other chains.

--- chains.js
+++ chains.js
@@ -52,36 +52,8 @@ Example.chains = function() {
     
     group = Body.nextGroup(true);
     
-    var ropeB = Composites.stack(350, 50, 10, 1, 10, 10, function(x, y) {
-        return Bodies.circle(x, y, 20, { collisionFilter: { group: group } });
-    });
-    
-    Composites.chain(ropeB, 0.5, 0, -0.5, 0, { stiffness: 0.8, length: 2, render: { type: 'line' } });
-    Composite.add(ropeB, Constraint.create({ 
-        bodyB: ropeB.bodies[0],
-        pointB: { x: -20, y: 0 },
-        pointA: { x: ropeB.bodies[0].position.x, y: ropeB.bodies[0].position.y },
-        stiffness: 0.5
-    }));
-    
-    group = Body.nextGroup(true);
-
-    var ropeC = Composites.stack(600, 50, 13, 1, 10, 10, function(x, y) {
-        return Bodies.rectangle(x - 20, y, 50, 20, { collisionFilter: { group: group }, chamfer: 5 });
-    });
-    
-    Composites.chain(ropeC, 0.3, 0, -0.3, 0, { stiffness: 1, length: 0 });
-    Composite.add(ropeC, Constraint.create({ 
-        bodyB: ropeC.bodies[0],
-        pointB: { x: -20, y: 0 },
-        pointA: { x: ropeC.bodies[0].position.x, y: ropeC.bodies[0].position.y },
-        stiffness: 0.5
-    }));
-    
     Composite.add(world, [
         ropeA,
-        ropeB,
-        ropeC,
         Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true })
     ]);
 
